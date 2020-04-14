1e2e530a5ea7a4c43238b41ceb7a4dd3b1b68c8e
Vlad Shcherban <vlad@coronalabs.com>
2020-04-14 00:40:00 -0400

    Adding binaries

---
+++ plugins/2017.3183/mac-sim/plugin_kochava_faa.lua
@@ -0,0 +1,55 @@
+-- Kochava FAA plugin
+
+local Library = require "CoronaLibrary"
+
+-- Create library
+local lib = Library:new{ name='plugin.kochava.faa', publisherId='com.coronalabs', version = 4 }
+
+-------------------------------------------------------------------------------
+-- BEGIN
+-------------------------------------------------------------------------------
+
+-- This sample implements the following Lua:
+--
+--    local PLUGIN_NAME = require "plugin_PLUGIN_NAME"
+--    PLUGIN_NAME:showPopup()
+--
+
+local function showWarning(functionName)
+    print( functionName .. "WARNING: The Kochava FAA plugin is only supported on Android, iOS and tvOS devices. Please build for device" );
+end
+
+function lib.getAttributionData()
+    showWarning("kochava.getAttributionData()")
+end
+
+function lib.init()
+    showWarning("kochava.init()")
+end
+
+function lib.limitAdTracking()
+    showWarning("kochava.limitAdTracking()")
+end
+
+function lib.logDeeplinkEvent()
+    showWarning("kochava.logDeeplinkEvent()")
+end
+
+function lib.logStandardEvent()
+    showWarning("kochava.logStandardEvent()")
+end
+
+function lib.logEvent()
+    showWarning("kochava.logEvent()")
+end
+
+function lib.setIdentityLink()
+    showWarning("kochava.setIdentityLink()")
+end
+
+-------------------------------------------------------------------------------
+-- END
+-------------------------------------------------------------------------------
+
+-- Return an instance
+return lib
